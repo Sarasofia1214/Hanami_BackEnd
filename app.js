3aa94f5f7c172e19bc5d799a5816fb2cfbf14da5
Sarasofia1214 <dasaro1206@gmail.com>
2025-12-07 00:22:32 -0500

    feat: :zap: Updating the url for swagger documentation

--- app.js
+++ app.js
@@ -4,16 +4,38 @@ import categoryRoutes from "./Routes/categoryRoutes.js";
 import ingredientRoutes from "./Routes/ingredientRoutes.js";
 import userRoutes from "./Routes/userRoutes.js";
 import orderRoutes from "./Routes/orderRoutes.js";
+import swaggerUi from "swagger-ui-express";
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+
+
+// Fix para rutas en ESModules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
-
 app.use(express.json());
+
 
 app.use("/dishes", dishRoutes);
 app.use("/categories", categoryRoutes);
 app.use("/ingredients", ingredientRoutes);
 app.use("/users", userRoutes);
 app.use("/orders", orderRoutes);
+// Cargar el swagger generado automáticamente
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(
+    JSON.parse(
+      fs.readFileSync(path.join(__dirname, "swagger", "swaggerAuto.json"), "utf8")
+    )
+  )
+)
+
+
 
 export default app;
 
